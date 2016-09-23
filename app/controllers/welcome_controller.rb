abacda882b1947388d319cdf220497a3bf32491a
Tyler Lugger <tyler.lugger@colorado.edu>
2016-09-22 22:01:25 -0600

    Added PDF

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,13 @@ class WelcomeController < ApplicationController
 
   # GET /welcome
   def index
-
+    def download_pdf
+      send_file(
+        "#{Rails.root}/app/assets/documents/Tyler_Lugger_Resume.pdf",
+        disposition: 'inline',
+        filename: "Tyler_Lugger_Resume.pdf",
+        type: "application/pdf"
+      )
+    end
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  get "welcome/download_pdf"
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
